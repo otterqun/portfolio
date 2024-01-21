34c6ddab8b6e7f99deaf6d42974c86e360c4deb1
otterqun <145894886+otterqun@users.noreply.github.com>
2024-01-21 17:01:16 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+window.addEventListener("scroll", function () {
+    toggleButtonVisibility();
+});
+
+// Toggle button visibility
+function toggleButtonVisibility() {
+    var button = document.getElementById("btn-to-top");
+    if (window.scrollY > 20) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
+
+// Smooth scroll to top
+function scrollToTop() {
+    var duration = 1500; // Adjust the duration as needed
+    var start = window.scrollY;
+    var startTime = performance.now();
+
+    function easeInOutQuart(t) {
+        return t < 0.5 ? 8 * t * t * t * t : 1 - Math.pow(-2 * t + 2, 4) / 2;
+    }
+
+    function scrollStep(timestamp) {
+        var timeElapsed = timestamp - startTime;
+        var progress = Math.min(timeElapsed / duration, 1);
+
+        var easedProgress = easeInOutQuart(progress);
+
+        window.scrollTo(0, start - start * easedProgress);
+
+        if (timeElapsed < duration) {
+            requestAnimationFrame(scrollStep);
+        }
+    }
+    
+    requestAnimationFrame(scrollStep);
+}
+
